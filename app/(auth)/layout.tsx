f8521575d2511796545dc5d5e05687ca714dc891
Roman <sheikhroman042@gmail.com>
2023-08-29 22:05:57 +0600

    auth done

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+import { ClerkProvider } from "@clerk/nextjs";
+
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <div className="flex items-center justify-center h-full">
+            {children}
+        </div>
+    );
+}
---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+const SatupPage =()=> {
+    return (
+        <div className="p-4">
+            <Button size="lg" variant="destructive">CLick</Button>
+      </div>
+    )
+  }
+  
